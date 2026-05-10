26ee1a3f4d3018b75ab805c8fc5ca1fd0c816c86
Clément Boyer <clement.boyer42360@gmail.com>
2026-05-10 14:11:58 +0200

    feat(argocd): let argocd manage himself (i.e create ingress/tls...) once other release (cert-manager, metallb, eso) are installed

--- terraform/helm_releases.tf
+++ terraform/helm_releases.tf
@@ -1,12 +1,9 @@
 resource "helm_release" "argocd" {
-  depends_on       = [helm_release.cert-manager]
   atomic           = true
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "9.4.4"
+  version          = "9.5.13"
   namespace        = "argocd"
   create_namespace = true
-  values           = [file("${path.module}/../helm/argocd/argocd-values.yaml")]
 }
-
