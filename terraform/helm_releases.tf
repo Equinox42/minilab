9291afbcb8e84a5ed11f6152d24ada5dbe8b1ac5
Clément Boyer <clement.boyer42360@gmail.com>
2026-05-18 19:28:48 +0200

    feat(terraform): add missing resources to apply app-of-apps manifest

--- terraform/helm_releases.tf
+++ terraform/helm_releases.tf
@@ -6,4 +6,12 @@ resource "helm_release" "argocd" {
   version          = "9.5.13"
   namespace        = "argocd"
   create_namespace = true
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "kubectl_manifest" "app_of_apps" {
+  depends_on = [ helm_release.argocd ]
+  yaml_body = file("${path.module}/../kubernetes/app-of-apps.yaml")
 }
